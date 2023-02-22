eec9c425e0323b1d82b0af7d2d854f91ffb8ed5f
Nekrasov Dmitriy <dimannekr@mail.ru>
2023-02-22 04:58:31 +0300

    Initial commit

---
+++ src/test/java/ru/netology/test/BankLogin.java
@@ -0,0 +1,27 @@
+package ru.netology.test;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.data.SQLHelper;
+import ru.netology.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.data.SQLHelper.cleanDatabase;
+
+import org.junit.jupiter.api.AfterAll;
+
+
+public class BankLogin {
+    @AfterAll
+    static void teardown() {cleanDatabase();
+    }
+
+    @Test
+    public void shouldSuccessLogin() {
+        open("http://localhost:9999");
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode);
+    }
+}
